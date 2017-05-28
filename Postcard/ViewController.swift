97369b188ddf417df9342781a97bf416b3df713d
Colin Mullikin <colin.mullikin@gmail.com>
2017-05-27 23:34:08 -0500

    Basic UI Controls
    Completed the first handful of videos up to the Github tutorial section

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(_ sender: UIButton) {
+        messageLabel.isHidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.red
+        
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", for: UIControlState.normal)
+    }
 
 }
 
